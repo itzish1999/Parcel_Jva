73e9e14e20c2c4ec48e87d64c6faed48b6e9efd0
itzish1999 <ismaeel.butt19@gmail.com>
2022-11-02 14:57:17 -0400

    feat(Database & Basic Setup): Setup the databas and changed application.prop. file to .yml

---
+++ Parcel/src/main/java/com/MTBtcmp/Parcel/ParcelApplication.java
@@ -0,0 +1,13 @@
+package com.MTBtcmp.Parcel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParcelApplication.class, args);
+	}
+
+}
---
+++ Parcel/src/test/java/com/MTBtcmp/Parcel/ParcelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MTBtcmp.Parcel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
